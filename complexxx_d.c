441e93b5196cb02164a07346026f971fb26af92f
Daria Levchenko <dashentus96@mail.ru>
2018-07-22 16:20:39 +0700

    Delete complexxx_d.c

--- complexxx_d.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "complexxx.h"
-
-complexxx divis(complexxx x, complexxx y){
-    complexxx d;
-    d.a = (x.a * y.a + x.b * y.b)/(y.a * y.a + y.b * y.b);
-    d.b = (y.a * x.b - x.a * y.b)/(y.a * y.a + y.b * y.b);
-    return d;
-}
